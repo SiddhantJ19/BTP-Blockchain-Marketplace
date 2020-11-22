519a6b1177e9219fa3c743ae330e6d3a5fcb7559
Adwait Thattey <coderdude1999@gmail.com>
2020-11-22 17:54:13 +0530

    add first query

--- Chaincode/DataChaincode/go/vendor/chaincode/queries.go
+++ Chaincode/DataChaincode/go/vendor/chaincode/queries.go
@@ -9,8 +9,40 @@ import "github.com/hyperledger/fabric-contract-api-go/contractapi"
 
 //// queryOnSaleDataMarketplace -> list of DevicePublicDetails onSale
 func (s *SmartContract) queryOnSaleDataMarketplace(ctx contractapi.TransactionContextInterface) ([]*DevicePublicDetails, error) {
+   queryString := fmt.Sprintf(`{"selector":{"onSale":true}}`)
+   resultsIterator, err := getQueryResultForQueryString(ctx, queryString)
+   if err != nil {
+		return nil, err
+	}
+   return constructPublicDevicesQueryResponseFromIterator(resultsIterator)
+}
 
-   return nil, nil
+func getQueryResultForQueryString(ctx contractapi.TransactionContextInterface, queryString string) (shim.StateQueryIteratorInterface, error) {
+	resultsIterator, err := ctx.GetStub().GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+   return resultsIterator, nil
+}
+
+func constructPublicDevicesQueryResponseFromIterator(resultsIterator) ([]*DevicePublicDetails, error) {
+	var assets []*DevicePublicDetails
+	for resultsIterator.HasNext() {
+		queryResult, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		var asset DevicePublicDetails
+		err = json.Unmarshal(queryResult.Value, &asset)
+		if err != nil {
+			return nil, err
+		}
+		assets = append(assets, &asset)
+	}
+
+	return assets, nil
 }
 
 //collection = MArketplace
